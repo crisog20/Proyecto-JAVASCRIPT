9463d913474ab0b0e80f13f89da157a01aadcd9c
crisog20 <cristinaostosgalan@gmail.com>
2019-04-23 10:15:10 +0200

    ultimos cambios de hoy en clase hay que cambiar el footer para ser fijo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("miSlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function openCity(evt, cityName) {
+  // Declarar las variables
+  var i, tabcontent, tablinks;
+
+  // Obtener todos los elementos con class = "tabcontent" y ocúltar
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Obtener todos los elementos con class = "tablinks" y eliminar la clase "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  //Muestra la pestaña actual y agrega una clase "activa" al botón que abrió la pestaña
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
 /***Acordeon***/
 var acc = document.getElementsByClassName("acordeon");
